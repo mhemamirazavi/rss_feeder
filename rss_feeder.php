0836be096891822725280b4efd0013767e2d2512
Mohammad Hossein Emami Razavi <mhemamirazavi@gmail.com>
2021-08-02 16:29:37 +0430

    adds jalali and BS RTL support

--- rss_feeder.php
+++ rss_feeder.php
@@ -1,9 +1,18 @@
-<html>
+<?php
+// 1. read this link to make bootstrap page RTL: 
+//  https://getbootstrap.com/docs/5.0/getting-started/rtl/
+// 2. see jalali project via link below:
+//  https://github.com/morilog/jalali
+require __DIR__ . '/vendor/autoload.php';
+$date = jdate();
+?>
+
+<html dir="rtl"  lang="fa">
 
 <head>
     <style>
         body {
-            font-family: Vazir;
+            font-family: Vazir !important;
             direction: rtl;
         }
 
@@ -19,7 +28,7 @@
         }
 
         @font-face {
-            font-family: Vazir;
+            font-family: Vazir im;
             src: url('./fonts/Vazir.eot');
             src: url('./fonts/Vazir.eot?#iefix') format('embedded-opentype'),
                 url('./fonts/Vazir.woff') format('woff'),
@@ -56,17 +65,19 @@ $cat = $_GET['cat'] ?? 0;
 define('CAT_GLOBAL', '0');
 define('CAT_SPORT', '1');
 define('CAT_Political', '2');
-define('CAT_Economical','3')
+define('CAT_Economical','3');
 $categories = [
     CAT_GLOBAL => 'عمومی',
     CAT_SPORT => 'ورزشی',
     CAT_Political =>'سیاسی',
-    CAT_Economical=>'اقتصادی'
+    CAT_Economical=>'اقتصادی',
 ];
 ?>
 <body>
+
     <form action="" method="get">
-        <select name="cat">
+        <div class="input-group">
+        <select name="cat" class="form-select">
             <?php 
             foreach ($categories as $key => $value) {
             ?>
@@ -74,9 +85,12 @@ $categories = [
             <?php 
             }
             ?>
+            
         </select>
-        <button type="submit">بارگذاری خبر</button>
+        <button type="submit" class="btn btn-outline-primary">بارگذاری خبر</button>
+    </div>    
     </form>
+
     <?php
     function get_content($URL)
     {
@@ -109,14 +123,14 @@ $categories = [
             $rss_urls = [
                 'مهر' => 'https://www.mehrnews.com/rss',
                 'فارس' => 'https://www.farsnews.ir/rss',
-                // 'تسنیم' => 'https://www.tasnimnews.com/fa/rss/feed/0/8/0/',
-                // 'ایرنا' => 'https://www.irna.ir/rss',
-                //'تابناک' => 'https://www.tabnak.ir/fa/rss/1',
-                // 'خبرفارسی' => 'https://khabarfarsi.com/rss/top',
-                // 'https://www.yjc.news/fa/rss/allnews',
-                // 'ایسنا' => 'https://www.isna.ir/rss',
-                // 'خبرآنلاین' => 'https://www.khabaronline.ir/rss',
-                // 'مشرق' => 'https://www.mashreghnews.ir/rss',
+                'تسنیم' => 'https://www.tasnimnews.com/fa/rss/feed/0/8/0/',
+                'ایرنا' => 'https://www.irna.ir/rss',
+                'تابناک' => 'https://www.tabnak.ir/fa/rss/1',
+                'خبرفارسی' => 'https://khabarfarsi.com/rss/top',
+                'https://www.yjc.news/fa/rss/allnews',
+                'ایسنا' => 'https://www.isna.ir/rss',
+                'خبرآنلاین' => 'https://www.khabaronline.ir/rss',
+                'مشرق' => 'https://www.mashreghnews.ir/rss',
             ];
             break;
         case CAT_SPORT: // varzeshi
@@ -127,8 +141,6 @@ $categories = [
                 'خبرانلاین' => 'https://www.khabaronline.ir/rss/tp/6',
                 'خبر فارسی' => 'https://khabarfarsi.com/rss/category/2518',
                 'مشرق' => 'https://www.mashreghnews.ir/rss?pl=99',
-                '' => '',
-                '' => '',
             ];
             break;
             case CAT_Political: //CIACI
@@ -142,26 +154,43 @@ $categories = [
                     'فارس نیوز' => 'https://www.farsnews.ir/rss/politics',
                 ];
                 break;
-    }
 
+                case CAT_Economical: //eghdesadi
+                    $rss_urls = [
+                          'تابناک' => 'https://www.tabnak.ir/fa/rss/6',
+                          'فارس نیوز' => 'https://www.farsnews.ir/rss/economy',
+                          'ایسنا' => 'https://www.isna.ir/rss/tp/34',
+                          'ایرنا' => 'https://www.irna.ir/rss/tp/20',
+                          'خبرانلاین' => 'https://www.khabaronline.ir/rss/tp/2',
+                          'خبرفارسی' => 'https://khabarfarsi.com/rss/category/2471',
+                          'مشرق' => 'https://www.mashreghnews.ir/rss/tp/16',
+                          
+                    ];
+                    break;
+    }
+?>
+<div class="m-3">
+<?php
     foreach ($rss_urls as $name => $rss_url) {
         $obj = simplexml_load_file($rss_url);
         $item = $obj->channel->item[0];
         $title = (string) $item->title;
         $link = (string) $item->link;
-        echo '<span class="pubdate">'.$obj->channel->item->pubDate.'</span>';
-        // var_dump([$title, $link]);
-        // $title (meghdar darad) == not false == true
-        // $title == '0'
+
+        $time = \Morilog\Jalali\CalendarUtils::strftime('l، Y/m/d H:i', strtotime($item->pubDate)); // 1395-02-19
+        $time = \Morilog\Jalali\CalendarUtils::convertNumbers($time);
         if ($title) {
     ?>
-            <a target="_blank" href="<?php echo $link; ?>" class="news-link">
+            <a target="_blank" title="<?php echo $time; ?>" href="<?php echo $link; ?>" class="news-link">
                 <?php echo $title; ?>
             </a> <span style="color: gray;"><?php echo $name; ?></span></br>
     <?php
         }
     }
     ?>
+    </div>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
